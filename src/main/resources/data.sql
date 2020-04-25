ea348945aaec8abfdea9ff84eaca3986788ab360
Rajat Verma <rverma@msewa.com>
2020-04-25 18:25:38 +0530

    h2 custom schema, authorities based

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
+insert into users (username, password, enabled)
+values
+('user', 'pass', true),
+('admin', 'pass', true);
+
+insert into authorities(username, authority)
+values
+('user', 'ROLE_USER'),
+('admin', 'ROLE_ADMIN');
